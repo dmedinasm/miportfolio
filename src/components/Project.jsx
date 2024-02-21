a988f5ca2ab53b8536ae7e3eb7c737fa26ab96f3
dmedinas <dmedinas@gmail.com>
2024-02-21 22:59:30 +0100

    Agregado archivo README y cambio de estilo en links repos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,8 @@ const Project = ({title,link,image,description, repoLink}) => {
           <h2 className="font-montserrat font-bold text-[130%] md:text-[200%]">{title}</h2>
           <a href={link} className="underline font-montserrat font-bold text-[85%] md:text-[110%]" target='_blank'>{link}</a>
           <div className="font-montserrat font-bold text-[85%] md:text-[110%]">
-          <p className="pr-6">github repo del proyecto:<a href={repoLink} className="underline pl-2 " target='_blank'>{repoLink}</a></p>
+          <p className="pr-6 mb-2">github repo del proyecto:</p>
+          <a href={repoLink} className="underline" target='_blank'>{repoLink}</a>
           </div>
           <img src={image} alt={`img ${title}`} className="w-[900px] rounded-xl shadow-xl" />
           <p className="font-montserrat  text-[100%] md:text-[130%] text-start leading-8 md:leading-10 ">{description}</p>
