e5f563302291361df54538c990bcacc66af52e58
dmedinas <dmedinas@gmail.com>
2024-03-01 18:50:48 +0100

    Agregadas opciones de navegacion alas secciones principales

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { IoMdCheckmark } from "react-icons/io";
 const AboutMe = () => {
+    
   return (
       <section className="py-12 px-8 md:px-[64px] flex flex-col gap-10 ">
-          <h2 className="font-montserrat font-bold  text-[200%] md:text-[300%] mb-4 text-slate-600 text-start"> Sobre Mí</h2>
+          <h2 className="font-montserrat font-bold  text-[200%] md:text-[300%] mb-4 text-slate-600  border-[2px] pl-8" id="aboutMe"> Sobre Mí</h2>
           <div>
-              <h3 className="font-montserrat font-bold text-[130%] md:text-[200%] text-slate-600 mb-2">Mi camino</h3>
+              <h3 className="font-montserrat font-bold text-[130%] md:text-[200%] text-slate-600 mb-2">Mi Camino</h3>
               <ul className="font-montserrat text-[100%] md:text-[130%] text-start leading-8 md:leading-10 flex flex-col gap-8 pl-4 items-start">
-                <li ><span className="font-bold text-slate-600 ">✔  2007 - Técnico Medio en Informática:</span> Adquirí las bases de la programación y el pensamiento lógico.</li>
-                <li ><span className="font-bold text-slate-600 ">✔ 2009-2014 - Carrera de Ingeniería en Telecomunicaciones:</span> Fortalecí mis conocimientos de lógica computacional a traves de materias como electrónica digital y lenguaje ensamblador aplicado a sistemas con microprocesadores.</li>
-                <li > <span className="font-bold text-slate-600 ">✔ 2014-2022 - Ingeniero en Telecomunicaciones:</span> Desarrollo habilidades de gestión y trabajo en equipo.</li>
-                <li ><span className="font-bold text-slate-600 ">✔ 2022-presente - Desarrollador Frontend:</span> Retomo mi pasión por la programación, enfocandome en React. He completado varios cursos y proyectos, y estoy listo para aportar valor a partir de los conocimientos adquiridos.</li>
+                <li ><span className="font-bold text-slate-600 ">  2007 - Técnico Medio en Informática:</span> Adquirí las bases de la programación y el pensamiento lógico.</li>
+                <li ><span className="font-bold text-slate-600 "> 2009-2014 - Carrera de Ingeniería en Telecomunicaciones:</span> Fortalecí mis conocimientos de lógica computacional a traves de materias como electrónica digital y lenguaje ensamblador aplicado a sistemas con microprocesadores.</li>
+                <li > <span className="font-bold text-slate-600 "> 2014-2022 - Ingeniero en Telecomunicaciones:</span> Desarrollo habilidades de gestión y trabajo en equipo.</li>
+                <li ><span className="font-bold text-slate-600 "> 2022-presente - Desarrollador Frontend:</span> Retomo mi pasión por la programación, enfocandome en React. He completado varios cursos y proyectos, y estoy listo para aportar valor a partir de los conocimientos adquiridos.</li>
               </ul>
           </div>
           <div>
               <h3 className="font-montserrat font-bold text-[130%] md:text-[200%] text-slate-600  mb-2">Mis Habilidades</h3>
               <ul className="font-montserrat text-[100%] md:text-[130%] text-start leading-8 md:leading-10 flex flex-col gap-4 pl-4">
-                  <li className="flex items-center gap-2"><span className="text-slate-600 font-bold">✔</span> HTML, CSS, JavaScript</li>
-                  <li className="flex items-center gap-2 "><span className="text-slate-600 font-bold">✔</span> ReactJS</li>
-                  <li className="flex items-center gap-2"><span className="text-slate-600 font-bold">✔</span> Responsive design</li>
-                  <li className="flex items-center gap-2"><span className="text-slate-600 font-bold">✔</span> Trabajo en equipo</li>
-                  <li className="flex items-center gap-2"><span className="text-slate-600 font-bold">✔</span> Autoaprendizaje</li>
+                  <li className="flex items-center gap-2"> HTML, CSS, JavaScript</li>
+                  <li className="flex items-center gap-2 "> ReactJS</li>
+                  <li className="flex items-center gap-2"> Responsive design</li>
+                  <li className="flex items-center gap-2"> Trabajo en equipo</li>
+                  <li className="flex items-center gap-2"> Autoaprendizaje</li>
               </ul>
           </div>
           <div>
--- src/components/SocialLogo.jsx
+++ src/components/SocialLogo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SocialLogo = ({logoSocial, link}) => {
+const SocialLogo = ({logoSocial, link, isOpening}) => {
   return (
     <>
-          <div className="border-[2px] md:border-[5px] border-solid border-black rounded-[50%] p-2 md:p-4 flex place-items-center hover:bg-gray-100 ">
+          <div className="border-[2px] md:border-[5px] border-solid border-black rounded-[50%] p-2 md:p-4 flex place-items-center hover:bg-gray-100" style={{backgroundColor: isOpening ? "#F4F4F5" :""}}>
             <a href={link} target='_blank'>
             {logoSocial}
             </a>  
